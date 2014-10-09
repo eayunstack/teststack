29c83aac224e0f7e7a9eebc3ac57b92c18e42b55
apporc <appleorchard2000@gmail.com>
2014-10-09 18:46:17 +0800

    fix heat wrong endpoint
    Signed-off-by: apporc <appleorchard2000@gmail.com>

--- install_heat.sh
+++ install_heat.sh
@@ -49,16 +49,16 @@ keystone service-create --name=heat --type=orchestration \
 --description="Orchestration"
 keystone endpoint-create \
 --service-id=$(keystone service-list | awk '/ orchestration / {print $2}') \
---publicurl=http://$CONTROLL_NAME:8004/v1/%\(tenant_id\)s \
---internalurl=http://$CONTROLL_NAME:8004/v1/%\(tenant_id\)s \
---adminurl=http://$CONTROLL_NAME:8004/v1/%\(tenant_id\)s
+--publicurl=http://$CONTROLLER_NAME:8004/v1/%\(tenant_id\)s \
+--internalurl=http://$CONTROLLER_NAME:8004/v1/%\(tenant_id\)s \
+--adminurl=http://$CONTROLLER_NAME:8004/v1/%\(tenant_id\)s
 keystone service-create --name=heat-cfn --type=cloudformation \
 --description="Orchestration CloudFormation"
 keystone endpoint-create \
 --service-id=$(keystone service-list | awk '/ cloudformation / {print $2}') \
---publicurl=http://$CONTROLL_NAME:8000/v1 \
---internalurl=http://$CONTROLL_NAME:8000/v1 \
---adminurl=http://$CONTROLL_NAME:8000/v1
+--publicurl=http://$CONTROLLER_NAME:8000/v1 \
+--internalurl=http://$CONTROLLER_NAME:8000/v1 \
+--adminurl=http://$CONTROLLER_NAME:8000/v1
 
 keystone role-create --name heat_stack_user
 
